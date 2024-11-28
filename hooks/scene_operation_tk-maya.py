54335a4901deb1a916415345e56099ab34acd3dd
junghyun_yeom <yjh881213@gmail.com>
2024-11-28 11:02:58 +0900

    export 확장자 ma->fbx로 수정

--- hooks/scene_operation_tk-maya.py
+++ hooks/scene_operation_tk-maya.py
@@ -14,12 +14,12 @@ class SceneOperation(HookBaseClass):
     current scene
     """
     
-    def _save_clean_file(self, file_path):
+    def _export_clean_fbx(self, file_path):
         """
-        Clean 모드로 파일을 저장하는 헬퍼 함수
+        Clean 모드로 FBX 파일을 내보내는 헬퍼 함수
         
         :param file_path: 원본 저장 경로
-        :return: 실제 저장된 파일 경로
+        :return: 실제 저장된 FBX 파일 경로
         """
         # clean 저장을 위한 새로운 폴더 경로 생성
         folder = os.path.dirname(file_path)
@@ -33,16 +33,47 @@ class SceneOperation(HookBaseClass):
             # 버전 번호가 있는 경우 처리
             base_name = name.split(".v")[0]
             version = name.split(".v")[1]
-            clean_name = f"{base_name}.clean.v{version}{ext}"
+            clean_name = f"{base_name}.clean.v{version}.fbx"
         else:
             # 버전 번호가 없는 경우 처리
-            clean_name = f"{name}.clean{ext}"
+            clean_name = f"{name}.clean.fbx"
         
-        # clean 폴더에 파일 저장
+        # clean 폴더에 FBX로 내보내기
         clean_path = os.path.join(clean_folder, clean_name)
-        cmds.file(rename=clean_path)
-        # mayaAscii 형식으로 강제 저장
-        cmds.file(save=True, force=True, type="mayaAscii")
+        
+        # FBX 내보내기 옵션 설정
+        mel.eval('FBXResetExport')  # FBX 내보내기 옵션 초기화
+        
+        # FBX 내보내기 기본 설정
+        mel.eval('FBXExportFileVersion -v FBX201800')  # FBX 2018 버전
+        mel.eval('FBXExportInputConnections -v 0')
+        mel.eval('FBXExportIncludeChildren -v 1')
+        mel.eval('FBXExportInAscii -v 1')  # ASCII 형식으로 내보내기
+        
+        # 애니메이션 설정
+        mel.eval('FBXExportAnimationOnly -v 0')
+        mel.eval('FBXExportBakeComplexAnimation -v 1')
+        mel.eval('FBXExportBakeComplexStart -v 0')
+        mel.eval('FBXExportBakeComplexEnd -v 1')
+        mel.eval('FBXExportBakeComplexStep -v 1')
+        
+        # 기하학 설정
+        mel.eval('FBXExportSmoothingGroups -v 1')
+        mel.eval('FBXExportHardEdges -v 0')
+        mel.eval('FBXExportTangents -v 0')
+        mel.eval('FBXExportSmoothMesh -v 1')
+        mel.eval('FBXExportInstances -v 0')
+        
+        # 기타 설정
+        mel.eval('FBXExportReferencedAssetsContent -v 1')
+        mel.eval('FBXExportUseSceneName -v 0')
+        
+        # FBX 파일로 내보내기
+        cmds.file(clean_path, force=True, exportAll=True, type="FBX export")
+        
+        # 현재 씬도 저장
+        cmds.file(save=True, force=True)
+        
         return clean_path
 
     def execute(self, operation, file_path, context, parent_action, file_version, read_only, **kwargs):
@@ -92,7 +123,7 @@ class SceneOperation(HookBaseClass):
             clean_enabled = kwargs.get("clean_enabled", False)
             
             if clean_enabled:
-                self._save_clean_file(file_path)
+                self._export_clean_fbx(file_path)
             else:
                 # 일반 저장
                 cmds.file(rename=file_path)
