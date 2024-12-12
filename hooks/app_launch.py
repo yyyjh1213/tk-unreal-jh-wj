8048b4072d086d91a829efa833d48972f17225f8
junghyun_yeom <yjh881213@gmail.com>
2024-12-12 17:41:02 +0900

    원진님 앱 런치로 수정

--- hooks/app_launch.py
+++ hooks/app_launch.py
@@ -21,6 +21,7 @@ import platform
 import tank
 
 
+
 ENGINES = {
     'tk-houdini' : 'houdini',
     'tk-maya' : 'maya',
@@ -30,6 +31,7 @@ ENGINES = {
     'tk-clarisse' : 'clarisse',
     'tk-unreal' : 'unreal'
 }
+
 
 
 class AppLaunch(tank.Hook):
@@ -62,56 +64,228 @@ class AppLaunch(tank.Hook):
         sg = self.tank.shotgun
         project = context.project
         user = context.user
-        
-        # 사용자 정보가 있는 경우에만 부서 정보를 조회
-        depart = None
-        if user:
-            depart = sg.find_one("Department", [['users', 'in', user]], ['name'])
+        depart = sg.find_one("Department", [['users', 'in', user]], ['name'])
 
-        # Check department permissions
         depart_confirm = False
-        if engine_name in ['tk-unreal', 'tk-maya']:
-            # Unreal Engine과 Maya는 모든 부서 사용 가능
-            depart_confirm = True
-        elif depart and depart.get('name'):
-            if depart['name'] == 'RND' and engine_name == 'tk-nuke':
-                depart_confirm = True
-            elif depart['name'] == 'General':
-                depart_confirm = True
 
-        # Handle UE special case for Python 3
+        if depart is not None:
+            if (depart['name'] == 'RND' and engine_name == 'tk-nuke') or depart['name'] in ['General']:
+                depart_confirm = True
+        else:
+            self.parent.log_debug("No department found for user: %s" % user)
+
         if sys.version_info.major == 3 and app_name == 'unreal' and system == 'Windows':
             now_dir = os.path.dirname(os.path.abspath(__file__))
             packages = os.path.join(now_dir, 'packages', 'win')
+
             sys.path.append(packages)
 
-            # Special handling for Unreal Engine
-            if app_name == 'unreal':
-                if app_args:
-                    cmd = '"%s" %s' % (app_path, app_args)
-                else:
-                    cmd = '"%s"' % app_path
-                exit_code = os.system(cmd)
-                return {"command": cmd, "return_code": exit_code}
-
-        # Default launch logic
-        if not depart_confirm:
-            return {"command": "", "return_code": 1}
-
-        if tank.util.is_linux():
-            cmd = "%s %s &" % (app_path, app_args)
-
-        elif tank.util.is_macos():
-            if app_path.endswith(".app"):
-                cmd = 'open -n -a "%s"' % (app_path)
-                if app_args:
-                    cmd += " --args %s" % app_args
+            external_paths = [
+                "external_path3",
+                packages
+            ]
+            
+            new_paths = os.pathsep.join(external_paths)
+            
+            if 'UE_PYTHONPATH' in os.environ:
+                os.environ['UE_PYTHONPATH'] += os.pathsep + new_paths
             else:
+                os.environ['UE_PYTHONPATH'] = new_paths
+
+            self.parent.log_debug("UNREAL ENGINE will be launched at WINDOWS OS")
+            self.parent.log_debug("HOOKS_APP_LAUNCH Updated Unreal Python paths:")
+            self.parent.log_debug("UE_PYTHONPATH: %s" % os.environ['UE_PYTHONPATH'])
+            self.parent.log_debug("sys.path: %s" % sys.path)
+
+        if depart_confirm:
+            
+            adapter = get_adapter(platform.system())
+            packages = get_rez_packages(sg, app_name, version, system, project)
+
+            try:
+                import rez as _
+            except ImportError:
+                rez_path = adapter.get_rez_module_root()
+                if not rez_path:
+                    raise EnvironmentError('rez is not installed and could not be automatically found. Cannot continue.')
+                
+                if sys.version_info.major == 3:
+                    rez_path = rez_path.decode('utf-8')
+                
+                sys.path.append(rez_path)
+            
+            from rez import resolved_context
+
+            if not packages or app_name == 'unreal':
+                if not packages:
+                    self.logger.debug('No rez packages were found. The default boot, instead.')
+                command = adapter.get_command(app_path, app_args)
+                return_code = os.system(command)
+                return {'command': command, 'return_code': return_code}
+            
+            context = resolved_context.ResolvedContext(packages)
+            return adapter.execute(context, app_args, app_name)
+
+        else:
+            if tank.util.is_linux():
+                # on linux, we just run the executable directly
                 cmd = "%s %s &" % (app_path, app_args)
 
-        else:  # Windows
-            cmd = 'start /B "App" "%s" %s' % (app_path, app_args)
+            elif tank.util.is_macos():
+                # If we're on OS X, then we have two possibilities: we can be asked
+                # to launch an application bundle using the "open" command, or we
+                # might have been given an executable that we need to treat like
+                # any other Unix-style command. The best way we have to know whether
+                # we're in one situation or the other is to check the app path we're
+                # being asked to launch; if it's a .app, we use the "open" command,
+                # and if it's not then we treat it like a typical, Unix executable.
+                if app_path.endswith(".app"):
+                    # The -n flag tells the OS to launch a new instance even if one is
+                    # already running. The -a flag specifies that the path is an
+                    # application and supports both the app bundle form or the full
+                    # executable form.
+                    cmd = 'open -n -a "%s"' % (app_path)
+                    if app_args:
+                        cmd += " --args %s" % app_args
+                else:
+                    cmd = "%s %s &" % (app_path, app_args)
 
-        # Run the command to launch the app
-        exit_code = os.system(cmd)
-        return {"command": cmd, "return_code": exit_code}
+            else:
+                # on windows, we run the start command in order to avoid
+                # any command shells popping up as part of the application launch.
+                cmd = 'start /B "App" "%s" %s' % (app_path, app_args)
+
+            # run the command to launch the app
+            exit_code = os.system(cmd)
+
+            return {"command": cmd, "return_code": exit_code}
+
+
+def get_rez_packages(sg, app_name, version, system, project):
+    
+    if system == 'Linux':
+        filter_dict = [['code','is',app_name.title()+" "+version],
+                       ['projects','in',project]
+                      ]
+        packages = sg.find("Software",filter_dict,['sg_rez'])
+        if packages : 
+            packages =  packages[0]['sg_rez']
+        else:
+            filter_dict = [['code','is',app_name.title()+" "+version],
+                        ['projects','is',None] ]
+            packages = sg.find("Software",filter_dict,['sg_rez'])
+            if packages:
+                packages =  packages[0]['sg_rez']
+
+    else:
+        filter_dict = [['code','is',app_name.title()+" "+version],
+                       ['projects','in',project]
+                      ]
+        packages = sg.find("Software",filter_dict,['sg_win_rez'])
+        if packages : 
+            packages =  packages[0]['sg_win_rez']
+        else:
+            filter_dict = [['code','is',app_name.title()+" "+version],
+                        ['projects','is',None] ]
+            packages = sg.find("Software",filter_dict,['sg_win_rez'])
+            if packages:
+                packages =  packages[0]['sg_win_rez']
+
+    if packages:
+        packages = [ x for x in packages.split(",")] 
+    else:
+        packages = None
+        
+    return packages
+
+
+
+def get_adapter(system=''):
+    if not system:
+        system = platform.system()
+    
+    options = {
+        'Linux' : LinuxAdapter,
+        'Windows' : WindowsAdapter
+        }
+
+    try :
+        return options[system]
+
+    except KeyError:
+        raise NotImplementedError('system "{system}" is currently unsupported. Options were, "{options}"'
+                                  ''.format(system=system, options=list(options)))
+    
+
+class BaseAdapter:
+
+    shell_type = 'bash'
+
+    @staticmethod
+    def get_command(path, args):
+        return 'mate-terminal -x bash -c "{path}" {args} &'.format(path=path, args=args)
+
+    @staticmethod
+    def get_rez_root_command():
+
+        return 'rez-env rez -- printenv REZ_REZ_ROOT'
+
+    @classmethod
+    def get_rez_module_root(cls):
+
+        command = cls.get_rez_root_command()
+        module_path, stderr = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
+
+        module_path = module_path.strip()
+
+        if not stderr and module_path:
+            return module_path
+
+        return ''
+
+    @classmethod
+    def execute(cls, context, args, command):
+
+        os.environ['USE_SHOTGUN'] = "OK"
+
+        if args:
+            command += ' {args}'.format(args=args)
+        
+        if platform.system() == 'Linux':
+            command = "mate-terminal -x bash -c '{}' &".format(command)
+        else:
+            command = "start /B 'App' '{}'".format(command)
+
+        context.execute_shell(
+            command = command,
+            stdin = False,
+            block = False
+        )
+
+        return_code = 0
+        context.print_info(verbosity=True)
+
+        return {
+            'command': command,
+            'return_code': return_code,
+        }
+
+
+class LinuxAdapter(BaseAdapter):
+
+    pass
+
+
+class WindowsAdapter(BaseAdapter):
+
+    shell_type = 'cmd'
+
+    @staticmethod
+    def get_command(path, args):
+        return 'start /B "App" "{path}" {args}'.format(path=path, args=args)
+
+    @staticmethod
+    def get_rez_root_command():
+
+        return 'rez-env rez -- echo %REZ_REZ_ROOT%'
